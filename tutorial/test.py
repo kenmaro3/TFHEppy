5907b04223f4b5a82d3904533643c8e295ee2656
akakou <akakou571@gmail.com>
2021-09-17 17:04:33 +0900

    [UPD] automated tests

--- tutorial/test.py
+++ tutorial/test.py
@@ -5,12 +5,27 @@ import tfheppy
 from tfheppy import Encoder
 from tfheppy import Ctxt
 from tfheppy import Service
+import pytest
+
+PERMIT_ERROR = 1/100
+PERMIT_ERROR2 = 1/3
+PERMIT_ERROR3 = 2/3
+
+encoder = Encoder(-10, 10, 32)
+
+ser = Service(encoder)
+ser.gen_keys()
 
 
 def sigmoid(a): return 1 / (1 + np.exp(-a))
 
 
-def test_hom_add_sub(ser):
+def assert_with_diff(x, y, permit=PERMIT_ERROR):
+    a = abs(x - y)
+    return np.all(a < permit)
+
+
+def test_hom_add_sub():
     x = 2
     y = 3
     c1 = ser.encode_and_encrypt(x)
@@ -18,9 +33,9 @@ def test_hom_add_sub(ser):
     c3 = ser.add_hom_fixed_encoder(c1, c2)
     c4 = ser.sub_hom_fixed_encoder(c1, c2)
     d = ser.decrypt_and_decode(c3)
-    print(f"{x+y} == {d}")
+    assert assert_with_diff(x + y, d)
     d = ser.decrypt_and_decode(c4)
-    print(f"{x-y} == {d}")
+    assert assert_with_diff(x - y, d)
 
     x = np.array([2, 3])
     y = np.array([1, 2])
@@ -29,79 +44,81 @@ def test_hom_add_sub(ser):
     c3 = ser.add_hom_fixed_encoder_vector(c1, c2)
     c4 = ser.sub_hom_fixed_encoder_vector(c1, c2)
     d = ser.decrypt_and_decode_vector(c3)
-    print(f"{x+y} == {d}")
+
+    assert assert_with_diff(x + y, d)
     d = ser.decrypt_and_decode_vector(c4)
-    print(f"{x-y} == {d}")
+    assert assert_with_diff(x - y, d)
 
 
-def test_ser_deser(ser):
+def test_ser_deser():
     x = 2
     c = ser.encode_and_encrypt(x)
     print("ser/deser ctxt via memory")
     cstr = ser.serialize_ctxt(c)
     c = ser.deserialize_ctxt(cstr)
     d = ser.decrypt_and_decode(c)
-    print(f"{x} == {d}")
+    assert assert_with_diff(x, d)
 
     print("ser/deser ctxt via file")
     ser.serialize_ctxt_to_file(c, "./c.data")
     c = ser.deserialize_ctxt_from_file("./c.data")
     d = ser.decrypt_and_decode(c)
-    print(f"{x} == {d}")
+    assert assert_with_diff(x, d)
 
     print("ser/deser sk via memory")
     skstr = ser.serialize_sk()
     ser.deserialize_sk(skstr)
     d = ser.decrypt_and_decode(c)
-    print(f"{x} == {d}")
+    assert assert_with_diff(x, d)
 
     print("ser/deser sk via file")
     ser.serialize_sk_to_file("./sk.key")
     ser.deserialize_sk_from_file("./sk.key")
     d = ser.decrypt_and_decode(c)
-    print(f"{x} == {d}")
+    assert assert_with_diff(x, d)
 
     print("ser/deser gk via memory")
     gkstr = ser.serialize_gk()
     ser.deserialize_gk(gkstr)
     c = ser.pbs_identity(c)
     d = ser.decrypt_and_decode(c)
-    print(f"{x} == {d}")
+
+    assert assert_with_diff(x, d, PERMIT_ERROR2)
 
     print("ser/deser gk via file")
     ser.serialize_gk_to_file("./gk.key")
     ser.deserialize_gk_from_file("./gk.key")
     c = ser.pbs_identity(c)
     d = ser.decrypt_and_decode(c)
-    print(f"{x} == {d}")
+    assert assert_with_diff(x, d, PERMIT_ERROR3)
 
 
-def test_pbs(ser):
+def test_pbs():
     x = 2
     c = ser.encode_and_encrypt(x)
     print("pbs_identity")
     c = ser.pbs_identity(c)
     d = ser.decrypt_and_decode(c)
-    print(f"{x} == {d}")
+    assert assert_with_diff(x, d, PERMIT_ERROR2)
 
     print("pbs_mult")
     m = 1.2
     c = ser.pbs_mult(c, m, 2)
     d = ser.decrypt_and_decode(c)
-    print(f"{x*m} == {d}")
+    assert assert_with_diff(x*m, d, PERMIT_ERROR3)
 
     print("pbs_relu")
     c = ser.pbs_relu(c)
     d = ser.decrypt_and_decode(c)
-    print(f"{x*m} == {d}")
+    assert assert_with_diff(x*m, d, PERMIT_ERROR2)
 
     print("pbs_sigmoid")
     c = ser.pbs_sigmoid(c)
     d = ser.decrypt_and_decode(c)
-    print(f"{sigmoid(x*m)} == {d}")
+    assert assert_with_diff(sigmoid(x*m), d, PERMIT_ERROR3)
 
 
-def test_enc_dec_vector(ser):
+def test_enc_dec_vector():
     x = [2 for _ in range(10)]
     t1 = time.time()
     c = ser.encode_and_encrypt_vector(x)
@@ -121,14 +138,14 @@ def test_enc_dec_vector(ser):
         print(el)
 
 
-def test_max(ser):
+def test_max():
     x1 = 2
     x2 = 3
     c1 = ser.encode_and_encrypt(x1)
     c2 = ser.encode_and_encrypt(x2)
     c3 = ser.max_hom(c1, c2)
     d = ser.decrypt_and_decode(c3)
-    print(f"{max(x1, x2)} == {d}")
+    assert assert_with_diff(max(x1, x2), d, PERMIT_ERROR2)
 
     x1 = [2, 3]
     x2 = [1, 2]
@@ -139,53 +156,49 @@ def test_max(ser):
     print(d)
 
 
-def test_sum_in_col(ser):
-    x = np.array([1, 2, 3])
-    c = ser.encode_and_encrypt_vector(x)
-    c = ser.add_in_col(c, 0, 3)
-    d = ser.decrypt_and_decode(c)
-    print(f"{np.sum(x)} == {d}")
+# def test_sum_in_col():
+#     x = np.array([1, 2, 3])
+#     c = ser.encode_and_encrypt_vector(x)
+#     c = ser.add_in_col(c, 0, 3)
+#     d = ser.decrypt_and_decode(c)
+#     print(f"{np.sum(x)} == {d}")
 
 
-def test_max_in_col(ser):
+def test_max_in_col():
     x = np.array([1, 2, 3])
     c = ser.encode_and_encrypt_vector(x)
     c = ser.max_in_col(c, 0, 3)
     d = ser.decrypt_and_decode(c)
     print(f"{np.max(x)} == {d}")
+    assert assert_with_diff(np.max(x), d, PERMIT_ERROR2)
 
 
-def test_inner(ser):
+def test_inner():
     x = np.random.rand(3)
     p = np.random.rand(3)
     c = ser.encode_and_encrypt_vector(x)
     c = ser.inner(c, p, 1)
     d = ser.decrypt_and_decode(c)
-    print(f"{np.inner(x, p)} == {d}")
+    assert assert_with_diff(np.inner(x, p), d, PERMIT_ERROR3)
 
 
-def test_matrix_vector_mult(ser):
+def test_matrix_vector_mult():
     x = np.random.rand(5)
     m = np.random.rand(3, 5)
     c = ser.encode_and_encrypt_vector(x)
     c = ser.vector_matrix_mult(c, m)
     d = ser.decrypt_and_decode_vector(c)
+
     print(f"{np.dot(m, x)}")
     print(d)
 
 
 if __name__ == "__main__":
-    print("setup encoder")
-    encoder = Encoder(-10, 10, 32)
-
-    print("setup service with keys")
-    ser = Service(encoder)
-    ser.gen_keys()
-
-    # test_hom_add_sub(ser)
-    # test_ser_deser(ser)
-    # test_pbs(ser)
-    # test_max(ser)
-    # test_sum_in_col(ser)
-    # test_max_in_col(ser)
-    # test_matrix_vector_mult(ser)
+    test_hom_add_sub()
+    test_hom_add_sub()
+    test_ser_deser()
+    test_pbs()
+    test_max()
+    # test_sum_in_col()
+    test_max_in_col()
+    test_matrix_vector_mult()
