81fe34cf743f66b17da83dc48a0ef98c7d73d7dc
akakou <akakou571@gmail.com>
2021-09-17 16:12:06 +0900

    [UPD] Auto format

--- include/ctxt.hpp
+++ include/ctxt.hpp
@@ -6,36 +6,38 @@
 #include <stdio.h>
 #include <cassert>
 
-namespace TFHEpp {
-using namespace std;
-
-class Ctxt
+namespace TFHEpp
 {
+  using namespace std;
+
+  class Ctxt
+  {
   public:
     //array<lvl0param::T, lvl0param::n+1> ctxt;
     TLWE<lvl0param> ctxt;
     Encoder encoder;
 
-
-    Ctxt(){
+    Ctxt()
+    {
     }
 
     //Ctxt(array<lvl0param::T, lvl0param::n+1> ctxt){
-    Ctxt(TLWE<lvl0param> ctxt, Encoder encoder){
+    Ctxt(TLWE<lvl0param> ctxt, Encoder encoder)
+    {
       this->ctxt = ctxt;
       this->encoder = encoder;
     }
-    
+
     //array<lvl0param::T, lvl0param::n+1> get() const noexcept{
-    TLWE<lvl0param> get() const noexcept{
+    TLWE<lvl0param> get() const noexcept
+    {
       return this->ctxt;
     }
 
-    Encoder get_encoder() const noexcept{
+    Encoder get_encoder() const noexcept
+    {
       return this->encoder;
     }
-};
+  };
 
-
-
-}//
+} //
